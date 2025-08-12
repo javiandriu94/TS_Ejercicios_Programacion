fbb0d5031cb1ca536bbcf2dce22cc549cb3dac9a
Javier Vasquez C. <javiandriu21@gmail.com>
2025-08-11 20:15:32 -0500

    Added reversedString function

---
+++ code/reversedString.ts
@@ -0,0 +1,22 @@
+const isLetter = (char: string):boolean => {
+    return char.toLowerCase() !== char.toUpperCase()
+}
+
+const reversedString = (text:string):string => {
+    let filtered:string = '';
+    let reversed:string = '';
+    let n = text.length
+    for (let i = 0; i < n; i++ ){
+        if(isLetter(text[i])){
+            filtered+=text[i]
+        }
+    }
+    n = filtered.length
+    for (let j = n-1; j >= 0; j--){
+        reversed += filtered[j]
+    }
+    return reversed;
+
+}
+
+console.log(reversedString('Hello world'))
