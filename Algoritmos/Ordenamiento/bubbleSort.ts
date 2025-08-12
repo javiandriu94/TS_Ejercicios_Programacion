d619f26e4d341ea3c9c3790b9cb0fb004a9aa5ca
Javier Vasquez C. <javiandriu21@gmail.com>
2025-08-11 19:47:37 -0500

    Updated bubbeSort function in ts

--- Algoritmos/Ordenamiento/bubbleSort.ts
+++ Algoritmos/Ordenamiento/bubbleSort.ts
@@ -1,12 +1,16 @@
-export const bubbleSort = (arr:number[]):number[] => {
-    for(let i:number = 0; i < arr.length; i++){
-        for(let j:number = 0 ; j < arr.length -i -1 ; j++){
-            if(arr[j] > arr[j+1]){
-                let temp:number = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+const bubbleSort = (arr:number[]): number[] => {
+    const sortedArray: number[] = [...arr];
+    const n = sortedArray.length;
+    for (let i = 0; i< n-1; i++){
+        for(let j = 0; j < n-i-1; j++){
+            if (sortedArray[j] > sortedArray[j + 1]) {
+                const temp = sortedArray[j];
+                sortedArray[j] = sortedArray[j + 1];
+                sortedArray[j + 1] = temp;
             }
-        }               
+
+        }
     }
-    return arr;
+    return sortedArray
 }
+console.log(bubbleSort([5, 3, 8, 4, 2]));
